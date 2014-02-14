6bc687dd69532f43b96d288f861febaaaecf67ec
Monika Solanki <monika.solanki@gmail.com>
2014-02-14 16:32:41 +0000

    A  commit to introduce the graph URI

--- src/main/java/org/purl/linkedepcis/eem/Namespaces.java
+++ src/main/java/org/purl/linkedepcis/eem/Namespaces.java
@@ -2,61 +2,80 @@ package org.purl.linkedepcis.eem;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import org.apache.log4j.Logger;
 import org.purl.linkedepcis.utils.EPCISPreferences;
 
-
 public class Namespaces {
 
-	// base IRI for events 
-	private String eventIRI;
-        Logger logger = Logger.getLogger(Namespaces.class);
-	
-	//a Map returning all the prefixes and IRIs
-	private Map<String, String> namespacePrefixes = new HashMap<String, String>();
-	private String baseIRI="";
-	public Namespaces(String baseURI)
-	{
-		this();
-		this.baseIRI=baseURI;
-		
-	
-	}
-	public Namespaces()
-	{
-		
-		setIRIandPrefix("eem", "http://purl.org/FIspace/eem#");
-		setIRIandPrefix("co", "http://purl.org/co#");
-		setIRIandPrefix("cbv", "http://purl.org/FIspace/cbv#");
-		setIRIandPrefix("vcard", "http://www.w3.org/2006/vcard/ns#");
-                setIRIandPrefix("wgs84", "http://www.w3.org/2003/01/geo/wgs84_pos#");
-	}
-	
-	
-	//return the eventIRI by appending the 
-	public String getNewEventIRI(String prefix) {
-		EPCISCommon epcs = new EPCISCommon();
-		eventIRI =namespacePrefixes.get(prefix).concat(prefix+EPCISPreferences.getNewEventCounter());
-		return eventIRI;
-	}
+    // base IRI for events 
+    private String eventIRI;
+    Logger logger = Logger.getLogger(Namespaces.class);
 
-	
-	//get the event IRI for  a specific prefix
-	public String getIRIForPrefix(String prefix)
-	{
-		return namespacePrefixes.get(prefix);
-	}
-	
-	
-	// set the base IRI for events and the prefix
-	public void setIRIandPrefix(String prefix, String eventIRI) {
-	
-		namespacePrefixes.remove(prefix);
-		namespacePrefixes.put(prefix, baseIRI.concat(eventIRI));
-	}
-	
+    //a Map returning all the prefixes and IRIs
+    private Map<String, String> namespacePrefixes;
+    private String baseIRI = "";
+    private String contextURI = "";
+
+    public String getContextURI() {
+        return namespacePrefixes.get("context");
+    }
+
+    public void setContextURI(String contextURI) {
+
+        namespacePrefixes.put("context", baseIRI.concat(contextURI) + "event" + EPCISPreferences.getEventCounter());
+    }
+
+    public Namespaces(String baseURI) {
+        this();
+        this.baseIRI = baseURI;
+
+    }
+
+    public Namespaces() {
+        namespacePrefixes = new HashMap<String, String>();
+        setIRIandPrefix("eem", "http://purl.org/FIspace/eem#");
+        setIRIandPrefix("co", "http://purl.org/co#");
+        setIRIandPrefix("cbv", "http://purl.org/FIspace/cbv#");
+        setIRIandPrefix("vcard", "http://www.w3.org/2006/vcard/ns#");
+        setIRIandPrefix("wgs84", "http://www.w3.org/2003/01/geo/wgs84_pos#");
+    }
+
+    //return the eventIRI by appending the 
+    public String getNewEventIRI(String prefix) {
+        EPCISCommon epcs = new EPCISCommon();
+//                logger.info(prefix);
+//                logger.info(EPCISPreferences.getNewEventCounter());
+//                logger.info(getIRIForPrefix(prefix));
+        eventIRI = getIRIForPrefix(prefix).concat(prefix + EPCISPreferences.getNewEventCounter());
+//                logger.info(eventIRI);
+        return eventIRI;
+    }
+
+    //get the event IRI for  a specific prefix
+    public String getIRIForPrefix(String prefix) {
+//             for (Entry<String, String> entry : namespacePrefixes.entrySet()) {
+//            String key=entry.getKey();
+//            String value=entry.getValue();
+//             System.out.println("key=" + entry.getKey() + ", value=" + entry.getValue());
+//        }
+//            logger.info(prefix);
+//          logger.info(namespacePrefixes.get(prefix));
+
+        return namespacePrefixes.get(prefix);
+    }
+
+    // set te base IRI for events and the prefix
+    public void setIRIandPrefix(String prefix, String eventIRI) {
+
+        //	namespacePrefixes.remove(prefix);
+        namespacePrefixes.put(prefix, baseIRI.concat(eventIRI));
+        //  logger.info(namespacePrefixes.get(prefix));
+    }
+
     // A map of namespace-to-prefix
-	public Map getNameSpacesPrefixes() {
-	   return namespacePrefixes;
-	}
+    public Map getNameSpacesPrefixes() {
+        return namespacePrefixes;
+    }
+
 }
